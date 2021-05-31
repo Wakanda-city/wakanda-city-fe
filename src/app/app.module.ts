d923bb91c9682e0db8acfffc38b66af2e39f95c4
Maabia <maabia.ferry16@gmail.com>
2021-05-31 18:03:56 -0300

    teste html css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RodapeComponent } from './rodape/rodape/rodape.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    RodapeComponent
+    RodapeComponent,
+
   ],
   imports: [
     BrowserModule,
