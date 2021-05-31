a300f32f8340f37aefa107558e31b1d3668ecbf1
Maabia <maabia.ferry16@gmail.com>
2021-05-31 19:56:00 -0300

    criacao componente, codigo html e css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RodapeComponent } from './rodape/rodape/rodape.component';
+import { QuemSomosComponent } from './historia/quem-somos/quem-somos.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RodapeComponent,
+    QuemSomosComponent,
 
   ],
   imports: [
