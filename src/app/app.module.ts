518d69971806077182d44208bd4cc449a8781a55
Alessandro Dias Jr <alessandro.dsjr@gmail.com>
2021-05-31 23:09:31 -0300

    Criação de rota do componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageModule } from './landing-page/landing-page.module';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule,
