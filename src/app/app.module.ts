996d5c69a8fbb98f04f7364b2a5488d690a0a658
breno39 <64810645+breno39@users.noreply.github.com>
2021-06-02 18:53:14 -0300

    adicionado dependencia ngx-mask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxMaskModule } from 'ngx-mask';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,7 @@ import { LandingPageModule } from './landing-page/landing-page.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LandingPageModule
+    LandingPageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { CadastroCidadaoComponent } from './components/cadastro-cidadao/cadastro-cidadao.component';
-
+import { NgxMaskModule } from 'ngx-mask';
 
 
 @NgModule({
@@ -13,7 +13,8 @@ import { CadastroCidadaoComponent } from './components/cadastro-cidadao/cadastro
     CadastroCidadaoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgxMaskModule.forRoot()
   ]
 })
 export class LandingPageModule { }
